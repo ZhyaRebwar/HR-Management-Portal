3c56f5f9c85dd5153906180e52c0348d2c10bcd9
Zhya Rebwar <ZhyaRebwar9@gmail.com>
2023-08-20 17:27:03 +0300

    Completing delete trait and using the authentications inside it to make sure of the right permissions.

--- Operations/DeleteTrait.php
+++ Operations/DeleteTrait.php
@@ -2,12 +2,44 @@
 
 namespace Operations;
 
+use Classes\CompareUserTitles;
+
 trait DeleteTrait
 {
 
     public function delete(?array $params): void
     {
-        
+        $params = $params['users'];
+
+        $this -> removeUser(...$params);
     }
 
+    public function removeUser(int $id_self, int $id_user, string $title_self, string $title_user): void
+    {
+        //1. check wether the user has the ability to delete this type of user.
+        $compareResult = (new CompareUserTitles) -> compareDelete($title_self, $title_user);
+        
+        //2. delete the user from the database
+        if( $compareResult )
+        {
+            $result = $this -> deleteUser($id_user);
+            echo json_encode( ['result' => 'record has been deleted'] );
+        }
+        else
+            echo json_encode( ['result' => 'error' ] );   
+    }
+
+    public function deleteUser(int $id ): void
+    {
+
+        $this->userObj->deleteUser();
+
+        $statement = $this->userObj->db->prepare( $this->userObj->query() );
+
+        $statement -> execute(
+            [
+                'id' => $id
+            ]);
+
+    }
 }
