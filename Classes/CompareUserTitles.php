1ad633196a44639327bc94b2c2288e51178ae2c9
Zhya Rebwar <ZhyaRebwar9@gmail.com>
2023-08-21 00:29:18 +0300

    adding delete to the compare class to be able to compare titles

---
+++ Classes/CompareUserTitles.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Classes;
+
+use Model\Users;
+use Constants\UserTitles;
+
+class CompareUserTitles
+{
+
+    private Users $userObj;
+    public function __construct()
+    {
+        $this->userObj = new Users();
+    }
+
+    public function compareView(string $title_self, string $title_user): bool
+    {
+        $viewAuth = UserTitles::checkAuthorizationView();
+        if( in_array( $title_user, $viewAuth[$title_self] ))
+            return true;            
+        
+        return false;   
+    }
+
+    public function compareDelete(string $title_self, string$title_user ): bool
+    {
+        $deleteAuth = UserTitles::checkAuthorizationDelete();
+        if( in_array( $title_user, $deleteAuth[$title_self] ))
+            return true;
+
+        return false;
+    }
+}
