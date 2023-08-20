32368b672ba97ddc0f0787a6f24fcb8ef63519cb
Zhya Rebwar <ZhyaRebwar9@gmail.com>
2023-08-21 00:23:30 +0300

    completing view trait

--- Operations/ViewTrait.php
+++ Operations/ViewTrait.php
@@ -2,6 +2,8 @@
 
 namespace Operations;
 
+use Classes\CompareUserTitles;
+
 trait ViewTrait
 {
 
@@ -28,15 +30,26 @@ trait ViewTrait
 
     public function viewUser(int $id_self, int $id_user, string $title_self, string $title_user): void
     {
-        echo "\nviewuser is activated";
         //1. now we create a class to identify the job of the self and user title
         //to be able to check whether the information is allowed to share or not.
-
+        $compareResult = (new CompareUserTitles) -> compareView($title_self, $title_user);
         
+        $checkEmployee = true;
         //1.5 aya employee'aka sar ba supervisor'akaya...    awa bo supervisor.
+        if( $title_self === 'supervisor' )
+            $checkEmployee = $this -> checkEmployee($id_self, $id_user);
+            
 
         //2. get the user from the database and do it by calling the view method.    
-        
+        if( $compareResult & $checkEmployee)
+        {
+            $result = $this->getUser($id_user );
+            echo json_encode( $result );
+        }
+        else
+            echo json_encode( ["Result" => 'Error' ]);
+
+
     }
 
     //done.
@@ -48,7 +61,7 @@ trait ViewTrait
         echo json_encode( $result );
     }
 
-    public function getUser(int $id, string $title): array
+    public function getUser(int $id ): array
     {
 
         $this->userObj->selectUser();
