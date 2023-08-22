07365a85c5ba273eefbbe594c25d6cb58a13c371
Zhya Rebwar <ZhyaRebwar9@gmail.com>
2023-08-22 09:58:41 +0300

    add exceptions to app.php file

--- Classes/App.php
+++ Classes/App.php
@@ -10,8 +10,8 @@ use Controller\AdministratorController;
 use Controller\HRController;
 use Controller\EmployeeController;
 use Controller\SupervisorController;
-use Exception;
-
+use Exceptions\NoUserException;
+use Exceptions\PathException;
 
 class App
 {
@@ -28,50 +28,64 @@ class App
     public function run(): void
     {
         
-
-        $method=strtolower($this->method);
-
-        $url = $this->url;
-
-        $url_id = explode("/", $url);
-
-        $body = (array) json_decode( file_get_contents('php://input'), true);
-
-        if( preg_match( '/[0-9]+/', $url ) )
+        try
         {
-            //gets the lever name
-            $id_user = $this->identify( (int) $url_id[2] );
 
-            //here we get the 4th index, so that we can later check whether the operation is on another is or same user.
-            if( (int) $url_id[4] )
-                $id_user2 = $this->identify( (int) $url_id[4] );
+            $method=strtolower( $this -> method);
 
-            $id = [
-                'id_self' => $url_id[2],
-                'id_user'=> $url_id[4],
-                'title_self' => $id_user,
-                'title_user' => $id_user2
-            ];
+            $url = $this -> url;
+
+            $url_id = explode("/", $url);
+
+            $body = (array) json_decode( file_get_contents('php://input'), true);
+
+            if( preg_match( '/[0-9]+/', $url ) )
+            {
+                //gets the lever name
+                $id_user = $this -> identify( (int) $url_id[2] );
+
+                //here we get the 4th index, so that we can later check whether the operation is on another is or same user.
+                if( (int) $url_id[4] )
+                    $id_user2 = $this -> identify( (int) $url_id[4] );
+
+                $id = [
+                    'id_self' => $url_id[2],
+                    'id_user'=> $url_id[4],
+                    'title_self' => $id_user,
+                    'title_user' => $id_user2
+                ];
 
 
-            $body['users'] = $id;
+                $body['users'] = $id;
 
-            $this -> getClass( $id_user );
-            
-            //making the uri match the routes
-            $url = preg_replace( "/[0-9]+/", '[id]', $url);
-        }
+                $this -> getClass( $id_user );
+                
+                //making the uri match the routes
+                $url = preg_replace( "/[0-9]+/", '[id]', $url);
 
-        if( !isset( $this->route[$method][$url] ) )
-        {
+                // if( !(bool) $id_user || !(bool) $id_user2 ) 
+                //     throw new NoUserException;
+                
+            }
+
+
+            $this -> routes();
+
             // throw exception(no routes found)
+            if( !isset( $this->route[$method][$url] ) )
+                throw new PathException;
+            
+
+            (new Route)->resolve( $this->route[$method][$url] , $method, $body);
         }
-
-  
-
-        $this -> routes();
-
-        (new Route)->resolve( $this->route[$method][$url] , $method, $body);
+        catch( PathException $e )
+        {
+            echo json_encode( ['Error' => $e -> errorMessage() ] );
+        }
+        catch( NoUserException $e)
+        {
+            echo json_encode( ['Error' => $e -> errorMessage() ] );
+        }
     }
 
     public function routes(): self
